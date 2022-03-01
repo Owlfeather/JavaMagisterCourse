8d9f2437890d402ccb376f4ddf795587342036bc
Polina Martynyuk <polinka1106@yandex.ru>
2022-03-01 18:40:46 +0300

    Lab4 task 1 done

---
+++ Lab4/src/task1/MainForMobile.java
@@ -0,0 +1,23 @@
+package task1;
+
+public class MainForMobile {
+    public static void main(String[] args) {
+
+        Mobile mobile = new Mobile("Xiaomi");
+
+        mobile.addModel("Mi 11");
+        mobile.models.get(0).addAttribute("RAM", 8);
+        mobile.models.get(0).addAttribute("ROM", 256);
+        mobile.models.get(0).addAttribute("Accumulator", 4600);
+        mobile.models.get(0).addAttribute("NFC");
+
+        mobile.addModel("Mi 8");
+        mobile.models.get(1).addAttribute("RAM", 6);
+        mobile.models.get(1).addAttribute("ROM", 128);
+        mobile.models.get(0).addAttribute("Accumulator", 3400);
+
+        System.out.println(mobile);
+
+
+    }
+}
---
+++ Lab4/src/task1/Mobile.java
@@ -0,0 +1,109 @@
+package task1;
+
+import java.util.ArrayList;
+
+public class Mobile {
+    String firm;
+    ArrayList<Model> models;
+
+    public Mobile() {
+        models = new ArrayList<>();
+    }
+
+    public Mobile(String firm) {
+        this.firm = firm;
+        models = new ArrayList<>();
+    }
+
+    public void addModel(String name){
+        models.add(new Model(name));
+    }
+
+    @Override
+    public String toString() {
+        return "Mobile \n{" +
+                "firm: '" + firm + '\'' +
+                "\nmodels: \n" + models +
+                '}';
+    }
+
+    class Model{
+        String name;
+        ArrayList<Attribute> attributes;
+
+        public Model() {
+            attributes = new ArrayList<>();
+        }
+
+        public Model(String name) {
+            this.name = name;
+            attributes = new ArrayList<>();
+        }
+
+        public void addAttribute(String name, int amount){
+            Attribute attribute = new Attribute(name, amount);
+            attributes.add(attribute);
+        }
+
+        public void addAttribute(String name){
+            Attribute attribute = new Attribute(name);
+            attributes.add(attribute);
+        }
+
+        @Override
+        public String toString() {
+            return "\n   Model{" +
+                    "name='" + name + '\'' +
+                    "\n       attributes=" + attributes + '\n' +
+                    "   }";
+        }
+
+        class Attribute{
+            String name;
+            int amount;
+
+            public Attribute() {
+            }
+
+            public Attribute(String name) {
+                this.name = name;
+                this.amount = -1;
+            }
+
+            public Attribute(String name, int amount) {
+                this.name = name;
+                this.amount = amount;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public int getAmount() {
+                return amount;
+            }
+
+            public void setAmount(int amount) {
+                this.amount = amount;
+            }
+
+            @Override
+            public String toString() {
+                if(amount != -1) {
+                    return "Attribute{" +
+                            "name='" + name + '\'' +
+                            ", amount=" + amount +
+                            '}';
+                } else {
+                    return "Attribute{" +
+                            "name='" + name +
+                            '}';
+                }
+            }
+        }
+    }
+}
